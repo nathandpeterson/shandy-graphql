7a2ab3142408565f9f2a56f03cb34a6c41076428
Nathan Daniel Peterson <natperson@gmail.com>
2018-02-12 14:01:01 -0800

    add methods for counting word frequency in paragraphs

--- models/TextModel.js
+++ models/TextModel.js
@@ -6,7 +6,7 @@ class TextModel {
   static async getOneText(path){
     return await fs.readFileSync(path, 'utf-8')
   }
-  static async breakIntoParagraphs(text){
+  static breakIntoParagraphs(text){
     const lines = text.split('\n')
     const paragraphs = {}
     let counter = 0
@@ -24,11 +24,44 @@ class TextModel {
     }
     return paragraphs
   }
+  static createOneFrequency(paragraph){
+    // Takes one paragraphs and returns a word frequency object
+    const wordFrequency = {}
+    for(let i = 0; i < paragraph.length; i++){
+      let line = paragraph[i]
+      // removes punctuation from line, leaving only alphabetic chars
+      let alphabetLine = line.replace(/[^\w\s]/g,' ')
+      let words = alphabetLine.split(' ')
+      for(let i = 0; i < words.length; i++){
+        if(!words[i]) continue
+        let word = words[i].toLowerCase()
+        if(!wordFrequency[word]){
+          wordFrequency[word] = 1
+        } else {
+          wordFrequency[word] += 1
+        }
+      }
+    }
+    return wordFrequency
+  }
+  static createMultipleFrequencies(...paragraphs){
+    // Takes an indeterminate amount of paragraphs and returns a word frequency object for all paragraphs
+    const wordFrequency = {}
+    paragraphs.forEach(paragraph => {
+      let p_freq = this.createOneFrequency(paragraph)
+      let p_keys = Object.keys(p_freq)
+      p_keys.forEach(key => {
+        (!wordFrequency[key]) ? wordFrequency[key] = p_freq[key] :
+        wordFrequency[key] += p_freq[key]
+      })
+    })
+    return wordFrequency
+  }
 }
 
 
-TextModel.getOneText('../texts/mrs_dalloway.txt')
-  .then(text => {
-      const paragraphs = TextModel.breakIntoParagraphs(text)
-      console.log(paragraphs)
-})
+// TextModel.getOneText('../texts/mrs_dalloway.txt')
+//   .then(text => {
+//       const paragraphs = TextModel.breakIntoParagraphs(text)
+//       TextModel.createMultipleFrequencies(paragraphs[2], paragraphs[3])
+// })
